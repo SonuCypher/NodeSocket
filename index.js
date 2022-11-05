a4f62ec8511a2484581183c08f81ed6fa760d0a9
Cypher890 <abdulsuhaib07@gmail.com>
2022-11-05 21:27:10 +0530

    added socket io

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from 'express'
+const app = express()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server, {cors: { origin:"*" }})
+
+app.set("view engine","ejs")
+
+app.length('/home', (req,res)=>{
+    res.render('home')
+})
+
+server.listen(3001,()=>{
+    console.log('server running...')
+})
+
+io.on('connection',(socket)=>{
+    console.log(`user connected ${socket.id}`)
+})
